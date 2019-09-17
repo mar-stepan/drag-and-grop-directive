0c0d15ac4aef659f2f4c0238484efc3b3aa1801b
mar-stepan <m.stepan@ukr.net>
2019-09-17 21:05:12 +0100

    changes directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drag-n-drop';
+  files: any = [];
+
+  uploadFile(event) {
+    for (let index = 0; index < event.length; index++) {
+      const element = event[index];
+      this.files.push(element.name);
+    }
+  }
+  deleteAttachment(index) {
+    this.files.splice(index, 1);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DragDropDirective } from './drag-drop.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragDropDirective
   ],
   imports: [
     BrowserModule
+  ],
+  exports: [
+    DragDropDirective
   ],
   providers: [],
   bootstrap: [AppComponent]
